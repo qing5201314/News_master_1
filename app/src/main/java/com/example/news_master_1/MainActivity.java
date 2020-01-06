36a8b00b1a73c51bfd7a4ecd115485e12f220ee1
qing5201314 <734504763@qq.com>
2020-01-06 18:13:37 +0800

    个人中心

---
+++ app/src/main/java/com/example/news_master_1/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.news_master_1;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
+
+
+
+
+
+
+
